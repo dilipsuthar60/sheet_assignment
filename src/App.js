188cbbc80228ca52af3e5a2b982cddbe90667905
dilipsuthar60 <dilipsuthar212000@gmail.com>
2023-12-29 23:35:16 +0530

    add click to highlight

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import CustomDataGrid from './component/CustomDataGrid';
-import './App.css';
-import data from './data.js'
+import CustomDataGrid from "./component/CustomDataGrid";
+import "./App.css";
+import data from "./data.js";
 
 function App() {
   const convertToTitle = (columnNumber) => {
-    let ans = ''
+    let ans = "";
     while (columnNumber > 0) {
-      let code = (--columnNumber) % 26
-      ans = String.fromCharCode(code + 65) + ans
-      columnNumber = (columnNumber - code) / 26
+      let code = --columnNumber % 26;
+      ans = String.fromCharCode(code + 65) + ans;
+      columnNumber = (columnNumber - code) / 26;
     }
-    return ans
+    return ans;
+  };
+  const tableId = "A";
+  if (!data.length) {
+    return;
   }
-  const tableId = 'A';
-  const rows = Array.from({ length: 100 }, (_, i) => i + 1);
+  const rows = Array.from({ length: 20 }, (_, i) => i + 1);
   const columns = Array.from({ length: 56 }, (_, i) => convertToTitle(i + 1));
-  const backgroundImage = '';
-  return <CustomDataGrid  tableId={tableId} rows={rows} columns={columns} data={data} backgroundImage={backgroundImage} />;
+  const backgroundImage =
+    "https://images.unsplash.com/photo-1682687982046-e5e46906bc6e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  return (
+    <CustomDataGrid
+      tableId={tableId}
+      rows={rows}
+      columns={columns}
+      data={data}
+      backgroundImage={backgroundImage}
+    />
+  );
 }
 
 export default App;
--- src/component/CustomDataGrid.js
+++ src/component/CustomDataGrid.js
@@ -1,83 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
+import React, { useState, useEffect } from "react";
+import "../App.css";
 
 const CustomDataGrid = ({ tableId, rows, columns, data, backgroundImage }) => {
-    const [selectedCells, setSelectedCells] = useState([]);
-    const [tooltipContent, setTooltipContent] = useState('');
-    const [currentActiveCell,setCurrentActiveCell]=useState(null)
+  const [selectedCells, setSelectedCells] = useState([]);
+  const [tooltipContent, setTooltipContent] = useState("");
+  const [currentActiveCell, setCurrentActiveCell] = useState(null);
 
-    const generateCellId = (row, column) => {
-        if (row < 10) { row = `0${row}` }
-        return `${tableId}#${row}${column}`;
-    };
+  const generateCellId = (row, column) => {
+    if (row < 10) {
+      row = `0${row}`;
+    }
+    return `${tableId}#${row}${column}`;
+  };
 
-    const handleCellClick = (row, column) => {
-        const cellId = generateCellId(row, column);
-        setCurrentActiveCell(cellId)
-        console.log("🚀 ~ file: CustomDataGrid.js:15 ~ handleCellClick ~ cellId:", cellId)
-        // const cellData = data.find((item) => item.id === cellId);
-    };
+  const handleCellClick = (row, column) => {
+    const cellId = generateCellId(row, column);
+    setCurrentActiveCell(cellId);
+    setSelectedCells([...selectedCells, cellId]);
+  };
 
-    const handleCellMouseOver = (row, column) => {
-        const cellId = generateCellId(row, column);
-        const cellData = data.find((item) => item.id === cellId);
-        if (cellData && cellData.type === 'S') {
-            console.log("set cell data", cellData.property)
-            setTooltipContent(cellData.property);
+  const handleCellMouseOver = (row, column) => {
+    const cellId = generateCellId(row, column);
+    const cellData = data.find((item) => item.id === cellId);
+    if (cellData && cellData.type === "S") {
+      setTooltipContent(cellData.property);
+    } else {
+      setTooltipContent("");
+    }
+  };
 
-        } else {
-            setTooltipContent('');
-        }
-    };
+  useEffect(() => {}, []);
 
-    useEffect(() => {
-    }, []);
-
-    return (
-        <div className="custom-data-grid">
-            <div className="table-header">
-                <div className="table-header-cell table-id">{currentActiveCell?currentActiveCell:tableId}</div>
-                {columns.map((column) => (
-                    <div key={column} className="table-header-cell">
-                        {column}
-                    </div>
-                ))}
-            </div>
-            <div className="table-body">
-                {rows.map((row) => (
-                    <div key={row} className="table-row">
-                        <div className="table-header-cell row-number">{row}</div>
-                        {columns.map((column) => {
-                            const cellId = generateCellId(row, column);
-                            const cellData = data.find((item) => item.id === cellId) || {};
-                            const isSelected = selectedCells.includes(cellId);
-                            const cellStyle = {
-                                backgroundColor: cellData.color || (isSelected ? '#ADD8E6' : '#FFFFFF'),
-                            };
-
-                            return (
-                                <>
-                                    <div
-                                        key={column+row}
-                                        className={`hover-text table-cell ${isSelected ? 'selected' : ''}`}
-                                        style={cellStyle}
-                                        onClick={() => handleCellClick(row, column)}
-                                        onMouseOver={() => handleCellMouseOver(row, column)}
-                                    >
-                                        {cellData.content || ''}
-                                        {tooltipContent && <span className="tooltip-text top">{tooltipContent}</span>}
-                                    </div>
-                                </>
-                            );
-                        })}
-                    </div>
-                ))}
-            </div>
-            {backgroundImage && (
-                <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
-            )}
+  return (
+    <div className="custom-data-grid">
+      <div className="table-header">
+        <div className="table-header-cell table-id">
+          {currentActiveCell ? currentActiveCell : tableId}
         </div>
-    );
+        {columns.map((column) => (
+          <div key={column} className="table-header-cell">
+            {column}
+          </div>
+        ))}
+      </div>
+      <div className="table-body">
+        {rows.map((row, index) => (
+          <div key={row + index} className="table-row">
+            <div className="table-header-cell row-number">{row}</div>
+            {columns.map((column) => {
+              const cellId = generateCellId(row, column);
+              const cellData = data.find((item) => item.id === cellId) || {};
+              const isSelected = selectedCells.includes(cellId);
+              const cellStyle = {
+                backgroundColor:
+                  cellData.color || (isSelected ? "#ADD8E6" : "#FFFFFF"),
+                cursor: "pointer",
+              };
+
+              return (
+                <>
+                  <div
+                    key={column + row}
+                    className={`hover-text table-cell ${
+                      isSelected ? "selected" : ""
+                    }`}
+                    style={cellStyle}
+                    onClick={() => handleCellClick(row, column)}
+                    onMouseOver={() => handleCellMouseOver(row, column)}
+                  >
+                    {cellData.content || ""}
+                    {tooltipContent && (
+                      <span className="tooltip-text top">{tooltipContent}</span>
+                    )}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+      {/* {backgroundImage && (
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        ></div>
+      )} */}
+    </div>
+  );
 };
 
 export default CustomDataGrid;
