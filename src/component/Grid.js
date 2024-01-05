eac3ecfd08346a423df0e89c495a311d6b03f2c9
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-01-05 21:46:41 +0530

    background image added in assest folder

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -1,6 +1,7 @@
 import React from "react";
 import jsonData from "../data.js";
 import CustomDataGrid from "./CustomDataGrid.js";
+import backgroundimg from "../assets/back-grond-1.jpg";
 
 const Grid = () => {
   const convertToTitle = (columnNumber) => {
@@ -13,13 +14,13 @@ const Grid = () => {
     return ans;
   };
   const tableId = "A";
-  const rows = Array.from({ length: 50 }, (_, i) => i + 1);
-  const columns = Array.from({ length: 80 }, (_, i) => convertToTitle(i + 1));
+  const rows = Array.from({ length: 99 }, (_, i) => i + 1);
+  const columns = Array.from({ length: 702 }, (_, i) => convertToTitle(i + 1));
   const width = "60px"; // your custum width
   const height = "60px"; // your custum height
-  const backgroundImage =
-    "https://images.unsplash.com/photo-1682687982046-e5e46906bc6e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  // const backgroundImage = "";
+  // const backgroundImage =
+  // "https://images.unsplash.com/photo-1682687982046-e5e46906bc6e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const backgroundImage = backgroundimg;
   return (
     <CustomDataGrid
       tableId={tableId}
